33c9ca7755638a64599eeadf24b693901e614a16
igoryuzo <igor.yuzovitskiy@gmail.com>
2025-05-10 12:48:34 -0400

    change prod url to staging in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dollarchain - A social coordination game.",
   description: "A social coordination game.",
-  metadataBase: new URL('https://www.dollarchain.xyz/'),
+  metadataBase: new URL('https://staging.dollarchain.xyz/'),
   openGraph: {
     title: "Dollarchain - A social coordination game.",
     description: "A social coordination game.",
-    url: 'https://www.dollarchain.xyz/',
+    url: 'https://staging.dollarchain.xyz/',
     siteName: 'Dollarchain',
     images: [
       {
-        url: 'https://www.dollarchain.xyz/images/dollarchain-logo.png',
+        url: 'https://staging.dollarchain.xyz/images/dollarchain-logo.png',
         width: 1200,
         height: 630,
       }
@@ -26,14 +26,14 @@ export const metadata: Metadata = {
   other: {
     'fc:frame': JSON.stringify({
       version: "next",
-      imageUrl: "https://www.dollarchain.xyz/images/dollarchain-logo.png",
+      imageUrl: "https://staging.dollarchain.xyz/images/dollarchain-logo.png",
       button: {
         title: "Join Waitlist",
         action: {
           type: "launch_frame",
           name: "Dollarchain",
-          url: "https://www.dollarchain.xyz/",
-          splashImageUrl: "https://www.dollarchain.xyz/images/dollarchain-logo.png",
+          url: "https://staging.dollarchain.xyz/",
+          splashImageUrl: "https://staging.dollarchain.xyz/images/dollarchain-logo.png",
           splashBackgroundColor: "#ffffff"
         }
       }
