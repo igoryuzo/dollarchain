6dc4241bbefce61bf9ab4e32fbce3ff37d896305
igoryuzo <igor.yuzovitskiy@gmail.com>
2025-05-15 11:43:10 -0400

    here we go!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     siteName: 'Dollarchain',
     images: [
       {
-        url: 'https://www.dollarchain.xyz/images/game-starts.png',
+        url: 'https://www.dollarchain.xyz/images/dollarchain-joker-begin.gif',
         width: 1200,
         height: 630,
       }
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
   other: {
     'fc:frame': JSON.stringify({
       version: "next",
-      imageUrl: "https://www.dollarchain.xyz/images/game-starts.png",
+      imageUrl: "https://www.dollarchain.xyz/images/dollarchain-joker-begin.gif",
       button: {
         title: "Start Game",
         action: {
